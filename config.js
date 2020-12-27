8a2354e671c9ca3e4ad5c74f03532c8f91447a01
apurva0692 <66519574+apurva0692@users.noreply.github.com>
2020-12-27 15:29:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,19 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyASyOjOtJla-X-b3io2eLoaUc_bIRFSIIc",
-    authDomain: "book-santa-app.firebaseapp.com",
-    databaseURL: "https://book-santa-app.firebaseio.com",
-    projectId: "book-santa-app",
-    storageBucket: "book-santa-app.appspot.com",
-    messagingSenderId: "69634746716",
-    appId: "1:69634746716:web:6fbbfc110fb4475365f999",
-    measurementId: "G-DLB7XC0JPL"
+  apiKey: "AIzaSyDxUwDLXKPIrOw2u03Edsf_-ej4Tm249Is",
+  authDomain: "booksanta-18b5b.firebaseapp.com",
+  projectId: "booksanta-18b5b",
+  storageBucket: "booksanta-18b5b.appspot.com",
+  messagingSenderId: "291263266613",
+  appId: "1:291263266613:web:91bc3aefce9c6b814b167c"
+
+    /* apiKey: "AIzaSyABlV7U-HuQNo04o_Wx1WTrFnJJXlRWvPs",
+    authDomain: "book-santa-2ca2e.firebaseapp.com",
+    projectId: "book-santa-2ca2e",
+    storageBucket: "book-santa-2ca2e.appspot.com",
+    messagingSenderId: "4549153722",
+    appId: "1:4549153722:web:f5a144e6d8642ea3eb4383" */
   };
 
 
